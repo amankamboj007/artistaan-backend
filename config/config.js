1597f0c9d2d8e3130559f9fc1ac71a163580e663
Aman kamboj <kamboj.aman32@gmail.com>
2021-12-02 23:31:50 +0530

    mongodb setup

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const config = {
     baseUrl: "/api/v1.0",
-    port: process.env.PORT || 6969
+    port: process.env.PORT || 6969,
+    mongo_url:process.env.MONGO_URL  
 }
 
 module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 
-router.get('/login', )
+router.get('/', )
 
 module.exports = router
