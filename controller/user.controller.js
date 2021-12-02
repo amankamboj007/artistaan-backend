8b8fe2ef2af63812737b4de711e28d8cba1c5cc2
Aman kamboj <kamboj.aman32@gmail.com>
2021-12-03 02:25:13 +0530

    mongo create added

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,8 +2,10 @@ const userService = require('../services/user.services')
 
 
 const adduser = async (req, res) => {
-
-    res.send(req.body)
+    req.body.userRole = 1
+    let resp = await userService.adduser(req.body)
+    console.log(resp)
+    res.status(200).send('user Added')
 }
 
 
